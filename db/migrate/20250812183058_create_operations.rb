2ad8bd03e792fec66819ad5812b2bd41932f9d50
asnganje <abd.nganje@gmail.com>
2025-08-12 21:41:14 +0300

    auto update schema and migrations

---
+++ db/migrate/20250812183058_create_operations.rb
@@ -0,0 +1,10 @@
+class CreateOperations < ActiveRecord::Migration[8.0]
+  def change
+    create_table :operations do |t|
+      t.string :name
+      t.decimal :amount
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.timestamps
+    end
+  end
+end
