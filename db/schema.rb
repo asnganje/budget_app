97c3de4a19c053fb52d54c8e331db1b27078ad20
asnganje <abd.nganje@gmail.com>
2025-08-31 10:51:21 +0300

    add user to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_12_183058) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_31_075020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_12_183058) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "operations", force: :cascade do |t|
@@ -43,5 +45,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_12_183058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "operations", "users", column: "author_id"
 end
