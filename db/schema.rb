39f7a6b53e251443b986e895542156ed173186b3
asnganje <abd.nganje@gmail.com>
2025-08-31 11:37:23 +0300

    run migration to add group to operations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_31_075020) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_31_083642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,7 +29,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_31_075020) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
     t.index ["author_id"], name: "index_operations_on_author_id"
+    t.index ["group_id"], name: "index_operations_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,5 +48,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_31_075020) do
   end
 
   add_foreign_key "groups", "users"
+  add_foreign_key "operations", "groups"
   add_foreign_key "operations", "users", column: "author_id"
 end
