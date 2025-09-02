756444cfe18d5378f9579fb65f3ea120a5b412c0
asnganje <abd.nganje@gmail.com>
2025-09-02 11:42:04 +0300

    add stimulus js code for close modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,11 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["overlay"]
   connect() {
+    console.log("loaded");
+    document.addEventListener("turbo:frame-load", (event) => {
+    if (event.target.id === "modal") {
+      this.modal = document.getElementById("modal")
+      console.log("Modal frame ready:", this.modal)
+    }
+  })
   }
 
   toggle() {
-    this.modalTarget.classList.toggle("hidden")
+    console.log("clicked");
+    console.log(`mine is...${document.getElementById("modal")}`);
+    this.overlayTarget.remove()
   }
 }
