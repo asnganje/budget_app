3ca8b247ae9a3ac48bd6c8ed37a1fefde6ab2a71
asnganje <abd.nganje@gmail.com>
2025-09-07 15:35:12 +0300

    skip multiple db selector

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module BudgetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.database_selector = nil
   end
 end
