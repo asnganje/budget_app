9a115aa324e70a51475e9df9ebeb498496530e75
asnganje <abd.nganje@gmail.com>
2025-09-07 16:20:04 +0300

    only use primary db

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module BudgetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_record.database_selector = nil
+    config.active_record.database_roles = [:primary]
   end
 end
